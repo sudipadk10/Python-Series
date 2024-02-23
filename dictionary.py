a1ca82f83f846b662bfd5f1efc533838177318bf
Sudip <adhikarisudip869@gmail.com>
2024-02-22 20:22:14 -0500

    List of Students

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+students =[
+    {"name": "Chandre" , "Sem" : "1stSem" , "batch" : "2080Batch"},      
+    {"name": "Bibesh" , "Sem" : "6thSem" , "batch" : "2077Batch"},
+    {"name": "Sumit" , "Sem" : "3rdSem" , "batch" : "2079Batch"},
+    {"name": "bandre" , "Sem" : "1stSem" , "batch" : "2080Batch"}
+
+]
+
+
+for s in students:
+    print(s["name"] , s["Sem"] , s["batch"] ,sep=",")
---
+++ list.py
@@ -0,0 +1,18 @@
+# students = [ "Ram" ,"Sumeet" ,"Bibesh"]
+
+# for  i in  range(len(students)):
+#     print(i+1 ,students[i])
+
+
+# Dictionary
+
+students = {
+    "Chandra" : "1stSem" ,
+    "Sumit" : "3rdSem" ,
+    "Bibesh" : "6thSem" ,
+    "Sinchan" : "6thSem",
+}
+
+
+for s in students:
+    print(s , students[s])
