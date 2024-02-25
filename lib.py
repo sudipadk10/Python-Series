be006f9fd2b6415977a978d7a8edc2b23b979117
Sudip <adhikarisudip869@gmail.com>
2024-02-24 21:47:38 -0500

    Creating own library

---
+++ lib.py
@@ -0,0 +1,11 @@
+def main():
+    hello("World")
+    goodbye("World")
+
+def hello(name):
+    print(f"Hello , {name}")
+def goodbye(name):
+    print(f"Goodbye , {name}")
+
+if __name__ == "__main__":
+    main()
---
+++ say.py
@@ -0,0 +1,6 @@
+import sys
+
+
+from lib import hello
+if len(sys.argv) == 2:
+    hello(sys.argv[1]) 
