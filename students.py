2df21492e38bda37467c3113e78b3e77a16aa3e2
Sudip <adhikarisudip869@gmail.com>
2024-02-26 20:39:43 -0500

    Dictionary in file

--- students.py
+++ students.py
@@ -1,5 +1,13 @@
+students = [] #Create empty list
 
 with open("students.csv") as file:
     for line in file:
-        row=line.rstrip().split(",")
-        print(f"{row[0]} lives in {row[1]}")git
+        name,house =line.rstrip().split(",")
+        student = { "name": name , "house" : house}                    #Create a dictionary
+        students.append(student)
+
+def get_name(student):       #you can get house either, if you wanna sort by house alphabet.
+    return student["name"]
+    #using keey we can choose which variable have to be sorted.
+for student in sorted(students , key=get_name , reverse= True):
+    print(f"{student['name']} lives in {student['house']} ")
