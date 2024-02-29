5ef79045589ae036d30f6b37d017f0486d594ce5
Sudip <adhikarisudip869@gmail.com>
2024-02-28 19:56:27 -0500

    Formating input names

---
+++ format.py
@@ -0,0 +1,7 @@
+import re
+name = input("Enter your name :").strip()
+  #paranthesis forms groups , := is walrus operator used for "if and only if" 
+if matches := re.search("^(.+), *(.+)$" , name):  # * indicates 0 or more repeatatioin of whitespace.
+   name = matches.group(2) + " " + matches.group(1)
+
+print(f"Hello {name}")
