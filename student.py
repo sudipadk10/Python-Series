e47acf24f689cec6be2f64f23f58131f1a601e74
Sudip <adhikarisudip869@gmail.com>
2024-02-29 20:09:39 -0500

    Using tuple to return values

--- student.py
+++ student.py
@@ -3,7 +3,7 @@ def main():
     print(f"{student[0]} is from {student[1]}")
 
 def student_data():
-    return (input("Name :"), input("House :")) #returning a tuple indicated by () . Its value can not be changed.
+    return (input("Name :"), input("House :"))  #returning a tuple indicated by () . Its value can not be changed.
                                                 #IF there was [] then it is list which value can be changed.
 if __name__ == "__main__":
     main()
